99edb20109fe56c4599c0874ef3456c6f01e9dd7
johncross00 <johncross.ktm@gmail.com>
2024-05-04 02:58:49 +0000

    drawerHeader

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:random_user/screen/pages/home_page.dart';
-import 'package:random_user/screen/widgets/bottomtab.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
