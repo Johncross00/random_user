85f8fa737b56a3b45989ce89f9f62be8ae77700b
johncross00 <johncross.ktm@gmail.com>
2024-05-04 03:10:37 +0000

    drawerHeader

--- lib/screen/widgets/drawer_but.dart
+++ lib/screen/widgets/drawer_but.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class DrawerBut extends StatelessWidget {
   const DrawerBut({super.key});
 
@@ -7,21 +8,39 @@ class DrawerBut extends StatelessWidget {
     return Drawer(
       child: ListView(
         children: [
-          _buildListTile(icon: Icons.library_add, title: 'title', subtitle: 'subtitle', isEnable: true),
           _buildDrawerHeader(),
           _buildDrawerHeader(),
-          _buildListTile(icon: Icons.zoom_in, subtitle: 'Click to go to zoom', title: 'Zoom', isEnable: true),
-          _buildListTile(icon: Icons.nat, subtitle: 'Click to Nat', title: '', isEnable: false),
+          _buildListTile(
+              icon: Icons.zoom_in,
+              subtitle: 'Click to go to zoom',
+              title: 'Zoom',
+              isEnable: true),
+          _buildListTile(
+              icon: Icons.library_add,
+              title: 'title',
+              subtitle: 'subtitle',
+              isEnable: true),
+          _buildListTile(
+              icon: Icons.nat,
+              subtitle: 'Click to Nat',
+              title: '',
+              isEnable: false),
           const Divider(color: Colors.lightGreenAccent, thickness: 5.0),
-          _buildListTile(icon: Icons.calendar_view_day, subtitle: 'Click to view the calendra', title: 'Calendar', isEnable: true),
+          _buildListTile(
+              icon: Icons.calendar_view_day,
+              subtitle: 'Click to view the calendra',
+              title: 'Calendar',
+              isEnable: true),
         ],
       ),
     );
-
-
   }
 
-  _buildListTile({required IconData icon, required String title, required String subtitle, required bool isEnable }) {
+  _buildListTile(
+      {required IconData icon,
+      required String title,
+      required String subtitle,
+      required bool isEnable}) {
     return ListTile(
       leading: Icon(icon),
       title: Text(title),
@@ -39,9 +58,15 @@ _buildDrawerHeader() {
             backgroundImage: AssetImage("assets/images/spotify.jpeg"),
             radius: 40,
           ),
-          SizedBox(height: 15,),
-          Text("SPOTIFY", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,)
-          )],
+          SizedBox(
+            height: 15,
+          ),
+          Text("SPOTIFY",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ))
+        ],
       ),
     ),
   );
