d75fe1321d37cd94533c96fc436ff9120f977f7e
johncross00 <johncross.ktm@gmail.com>
2024-05-05 01:48:56 +0000

    row container

---
+++ lib/screen/widgets/row_container.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Container buildContainer({required double width, required Color color}) {
+  return Container(
+    width: width,
+    color: color,
+  );
+}
