09a25bf689b755107cf8f5073544d5f85cba1df6
johncross00 <johncross.ktm@gmail.com>
2024-05-07 00:32:55 +0000

    yes

--- lib/screen/pages/play_page.dart
+++ lib/screen/pages/play_page.dart
@@ -1,9 +1,20 @@
+
 import 'package:flutter/material.dart';
+import 'package:random_user/screen/widgets/bottom_sheet.dart';
 class PlayPage extends StatelessWidget {
-  const PlayPage({super.key});
+  const PlayPage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Expanded(
+      child: Container(
+        color: Colors.deepPurple,
+        child: const Center(
+          child: BottomSheetBar(),
+        ),
+      ),
+    );
   }
 }
