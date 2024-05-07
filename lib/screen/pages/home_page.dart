edae3982764752c700b3365065b36fca602ed878
johncross00 <johncross.ktm@gmail.com>
2024-05-07 00:32:27 +0000

    yes

--- lib/screen/pages/home_page.dart
+++ lib/screen/pages/home_page.dart
@@ -6,10 +6,8 @@ import 'package:random_user/screen/pages/play_page.dart';
 import 'package:random_user/screen/widgets/bottomtab.dart';
 import 'package:random_user/screen/widgets/drawer_but.dart';
 
-import '../widgets/row_container.dart';
-
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -18,11 +16,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late PageController _pageController;
   int _currentIndex = 0;
+
   @override
   void initState() {
     super.initState();
     _pageController = PageController(initialPage: _currentIndex);
   }
+
   @override
   void dispose() {
     _pageController.dispose();
@@ -44,19 +44,27 @@ class _HomePageState extends State<HomePage> {
       ),
       body: PageView(
         controller: _pageController,
-        onPageChanged: (index){
+        onPageChanged: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
-        children: <Widget>[
-          Container(color: Colors.red, height: 200,),
-          Container(color: Colors.black, height: 200,),
-          Container(color: Colors.orange, height: 200,),
-          Container(color: Colors.purple, height: 200,)
+        children: const <Widget>[
+          ExemplePage(),
+          PlayPage(),
+          PausePage(),
+          NextPage(),
         ],
       ),
-      bottomNavigationBar: const BotTab(),
+      bottomNavigationBar: BotTab(
+        selectedIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            _pageController.jumpToPage(index);
+          });
+        },
+      ),
       drawer: const DrawerBut(),
     );
   }
