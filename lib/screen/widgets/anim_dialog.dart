ee3027524215b283a3cd862085e824feb63983a9
johncross00 <johncross.ktm@gmail.com>
2024-05-21 12:46:58 +0000

    anim dialog

---
+++ lib/screen/widgets/anim_dialog.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class AnimDialog extends StatelessWidget {
+  const AnimDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: (){},
+        child: Text("Open Dialog Animation"));
+  }
+}
